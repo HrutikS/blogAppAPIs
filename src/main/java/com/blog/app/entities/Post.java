2641670c1c492fe115899bb22761e49795e680b4
HrutikS <hrutiksurwade.work@gmail.com>
2023-11-19 04:55:20 +0530

    Added Post Entity

--- src/main/java/com/blog/app/entities/Post.java
+++ src/main/java/com/blog/app/entities/Post.java
@@ -1,5 +1,33 @@
 package com.blog.app.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="posts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Post {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="post_id")
+	private Integer postId;
+	
+	@Column(name="title", length = 100, nullable = false)
+	private String postTitle;
+	
+	@Column(name="content")
+	private String postContent;
 
 }
